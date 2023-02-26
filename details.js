e0a9f80a7c3da1f53d9195484b0ecf452f42a668
Dana Amerio <danaamerio07@gmail.com>
2023-02-26 19:31:41 -0300

    ultimas correscciones

--- details.js
+++ details.js
@@ -15,15 +15,16 @@ contenedorCars.innerHTML = carsGeneradas
 function crearCars(eventosPasados){
   let cars = ''
   for (const event of eventosPasados){
-    cars += `<div class="card mb-3" style="max-width: 540px;">
+    cars += `<div class="card mb-3" style="max-width: 700px;">
     <div class="row g-0">
       <div class="col-md-4">
         <img src="${event.image}" class="img-fluid rounded-start" alt="..."/>
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>  
+          <h5 class="card-title">${event.name}</h5>
+          <p class="card-text">${event.description}</p>
+          <p class="card-text">${event.place}</p>
         </div>
       </div>
     </div>
